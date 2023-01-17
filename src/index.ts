00a6d487c6962029a4ab86de84cd7d06b6cb89a0
JoShMiQueL <joshmiquel.cs@gmail.com>
2023-01-17 05:55:24 +0100

    Main function done!

---
+++ src/index.ts
@@ -0,0 +1,231 @@
+import axios from "axios";
+import { load } from "cheerio";
+import fs from "fs";
+
+interface God {
+  name: string;
+  summary: {
+    title: string;
+    pantheon: string;
+    type: string;
+    class: string;
+    pros: string;
+    difficulty: string;
+    release_date: string;
+    favor: number;
+    gems: number;
+    voicelines: string;
+    voice_actor: string;
+  };
+  stats: {
+    health: string;
+    mana: string;
+    speed: string;
+    range: string;
+    attack_speed: string;
+    basic_attack: {
+      damage: string;
+      progression: string;
+    };
+    protecion: {
+      physical: string;
+      magical: string;
+    };
+    regen: {
+      hp5: string;
+      mp5: string;
+    };
+  };
+}
+
+const baseUrl = "https://smite.fandom.com";
+
+/**
+ * Remove all non-alphanumeric characters.
+ * @param string String to be normalized
+ * @returns {string} Normalized string
+ */
+function normalize(string: string): string {
+  try {
+    return string.replace(/\n/g, "").trim();
+  } catch (error) {
+    if (string === undefined) {
+      return "/undefined";
+    }
+    throw error;
+  }
+}
+
+/**
+ *  Get the list of all url's from the gods.
+ * @returns {Promise<string[]>} The list of all url's from the gods.
+ */
+async function getListOfGodsUrl(): Promise<string[]> {
+  const url = `${baseUrl}/wiki/List_of_gods`;
+  const response = await axios.get(url);
+  const $ = load(response.data);
+  const arrayGods = $(".blue-window > tbody > tr:not(:nth-child(1))")
+    .map(
+      (i, el) => `${baseUrl}${$(el).find("td:nth-child(2) > a").attr("href")}`
+    )
+    .toArray();
+  return arrayGods;
+}
+
+/**
+ * Get the json from the god.
+ * @param url The url of the god.
+ * @returns {Promise<God>} The json from the god.
+ */
+async function getGod(url: string): Promise<God> {
+  try {
+    const response = await axios.get(url);
+    const $ = load(response.data);
+    const godObject = {
+      name: normalize($(".title").text()),
+      summary: {
+        title: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > b:nth-child(1)"
+          ).text()
+        ),
+        pantheon: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2)"
+          ).text()
+        ),
+        type: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(2)"
+          ).text()
+        ),
+        class: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2)"
+          ).text()
+        ),
+        pros: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(8) > td:nth-child(2)"
+          ).text()
+        ),
+        difficulty: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(9) > td:nth-child(2)"
+          ).text()
+        ),
+        release_date: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(10) > td:nth-child(2)"
+          ).text()
+        ),
+        favor: Number(
+          normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(11) > td:nth-child(2)"
+            )
+              .text()
+              .replace(",", "")
+          )
+        ),
+        gems: Number(
+          normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(12) > td:nth-child(2)"
+            ).text()
+          )
+        ),
+        voicelines:
+          baseUrl +
+          normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(13) > td:nth-child(2) > a"
+            ).attr("href")!
+          ),
+        voice_actor: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(14) > td:nth-child(2)"
+          ).text()
+        ),
+      },
+      stats: {
+        health: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(16) > td:nth-child(2) > font:nth-child(1)"
+          ).text()
+        ),
+        mana: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(17) > td:nth-child(2) > font:nth-child(1)"
+          ).text()
+        ),
+        speed: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(18) > td:nth-child(2)"
+          ).text()
+        ),
+        range: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(19) > td:nth-child(2)"
+          ).text()
+        ),
+        attack_speed: normalize(
+          $(
+            ".infobox > tbody:nth-child(1) > tr:nth-child(20) > td:nth-child(2)"
+          ).text()
+        ),
+        basic_attack: {
+          damage: normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(22) > td:nth-child(2)"
+            ).text()
+          ),
+          progression: normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(23) > td:nth-child(2)"
+            ).text()
+          ),
+        },
+        protecion: {
+          physical: normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(25) > td:nth-child(2)"
+            ).text()
+          ),
+          magical: normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(26) > td:nth-child(2)"
+            ).text()
+          ),
+        },
+        regen: {
+          hp5: normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(28) > td:nth-child(2)"
+            ).text()
+          ),
+          mp5: normalize(
+            $(
+              ".infobox > tbody:nth-child(1) > tr:nth-child(29) > td:nth-child(2)"
+            ).text()
+          ),
+        },
+      },
+    };
+    return godObject;
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Failed to get god ${url}`);
+  }
+}
+
+/**
+ * Get the json array from the gods.
+ * @returns {Promise<God[]>} The json array from the gods.
+ */
+async function getListOfGods(): Promise<God[]> {
+  const listOfGodsUrl = await getListOfGodsUrl();
+  return Promise.all(listOfGodsUrl.map((url) => getGod(url)));
+}
+
+fs.writeFileSync("./gods.json", JSON.stringify(await getListOfGods(), null, 2));
